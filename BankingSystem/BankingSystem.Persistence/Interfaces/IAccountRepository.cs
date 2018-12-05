deb94326656541678cf8544f84d325c28d8104df
Thuan <johnny21042010@gmail.com>
2018-12-05 19:50:06 +0800

    removed Class1.cs. Added BaseController for Mediator. Added AccountRepository. Added StringExtension

---
+++ BankingSystem/BankingSystem.Persistence/Interfaces/IAccountRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem.Persistence.Interfaces
+{
+    public interface IAccountRepository
+    {
+        decimal GetCurrentBalanceByAccountId(Guid accountId);
+    }
+}
---
+++ BankingSystem/BankingSystem.Persistence/Repositories/AccountRepo.cs
@@ -0,0 +1,17 @@
+﻿using BankingSystem.Persistence.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingSystem.Persistence.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+
+
+        public decimal GetCurrentBalanceByAccountId(Guid accountId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
