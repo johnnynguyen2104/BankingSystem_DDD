3b1c11cb4e7452cd2ea3d3d5bf19861adcfa2cbf
Thuan <johnny21042010@gmail.com>
2018-12-07 19:15:53 +0800

    Added .NET Core Client

--- BankingSystem/BankingSystem.ClientTest/Program.cs
+++ BankingSystem/BankingSystem.ClientTest/Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace BankingSystem.Client
+namespace BankingSystem.ClientTest
 {
     class Program
     {
@@ -122,7 +116,7 @@ namespace BankingSystem.Client
 
         public decimal Amount { get; set; }
     }
-       
+
     public class ResponseAccountInfo
     {
         public int AccountId { get; set; }
